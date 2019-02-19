a69239c0c8bd8282a01f95c2c621a94c1123d5f0
D Schirde <dschirde@uni-potsdam.de>
2019-02-19 16:56:43 +0100

    Creating a setup.py
    [ds: adding required packages for installations and tests]

--- setup.py
+++ setup.py
@@ -3,21 +3,22 @@ import setuptools
 long_description = open("README.md").read()
 
 setuptools.setup(
-    name="minima-test-zbyszek",
+    name="example-pkg-maximafinder-dschirde",
     version="0.0.1",
-    author="Zbigniew Jędrzejewski-Szmek",
-    author_email="zbyszek@in.waw.pl",
-    description="A library to find function maxima",
-
+    author="D Schirde",
+    author_email="dschirde@uni-potsdam.de",
+    description="A small example package with a routine to find the index of the local maxima of a list",
     long_description=long_description,
     long_description_content_type="text/markdown",
-
-    url="https://github.com/potsdam-python-workshop/testing",
+    url="https://github.com/potsdam-python-workshop/git",
     packages=['maxima'],
-
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+
+    install_requires = ['numpy >= 20', 'scipy < 15.3'],
+
+    tests_require = ['pytest']
 )
